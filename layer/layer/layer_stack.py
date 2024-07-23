5434f8a6de5c316dfa1773b1614bd82ee29e7c10
Leandro Damascena <lcdama@amazon.pt>
2024-07-23 13:41:58 +0100

    adding github

--- layer/layer/layer_stack.py
+++ layer/layer/layer_stack.py
@@ -14,7 +14,7 @@ from aws_cdk import (
 )
 from aws_cdk.aws_lambda import Architecture, CfnLayerVersionPermission
 from aws_cdk.aws_ssm import StringParameter
-from cdk_aws_lambda_powertools_layer import LambdaPowertoolsLayer
+from leo_cdk_aws_lambda_powertools_layer import LambdaPowertoolsLayer
 from constructs import Construct
 
 
