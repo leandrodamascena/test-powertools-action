d10aff981fc4b1b5646871a85de38074ff080b72
Leandro Damascena <lcdama@amazon.pt>
2024-07-27 16:42:13 +0100

    adding github

--- layer/app.py
+++ layer/app.py
@@ -10,8 +10,8 @@ app = cdk.App()
 POWERTOOLS_VERSION: str = app.node.try_get_context("version")
 PYTHON_VERSION: str = app.node.try_get_context("pythonVersion")
 p86 = PYTHON_VERSION.replace(".", "")
-SSM_PARAM_LAYER_ARN: str = f"/layers/powertools-layer-v2-arn-{p86}"
-SSM_PARAM_LAYER_ARM64_ARN: str = f"/layers/powertools-layer-v2-arm64-arn-{p86}"
+SSM_PARAM_LAYER_ARN: str = f"/layers/powertools-layer-v3-x86-arn-{p86}"
+SSM_PARAM_LAYER_ARM64_ARN: str = f"/layers/powertools-layer-v3-arm64-arn-{p86}"
 
 if not POWERTOOLS_VERSION:
     raise ValueError(
@@ -22,7 +22,7 @@ if not POWERTOOLS_VERSION:
 
 LayerStack(
     app,
-    f"LayerV2Stack-{p86}",
+    f"LayerV3Stack-{p86}",
     powertools_version=POWERTOOLS_VERSION,
     python_version=PYTHON_VERSION,
     ssm_parameter_layer_arn=SSM_PARAM_LAYER_ARN,
@@ -31,7 +31,7 @@ LayerStack(
 
 CanaryStack(
     app,
-    f"CanaryV2Stack-{p86}",
+    f"LayerV3Stack-{p86}",
     powertools_version=POWERTOOLS_VERSION,
     python_version=PYTHON_VERSION,
     ssm_paramter_layer_arn=SSM_PARAM_LAYER_ARN,
